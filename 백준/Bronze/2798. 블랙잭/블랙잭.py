b71a59153162ed2bd73e87606bd0ae0f5a910031
최진실 <63334368+chlwlstlf@users.noreply.github.com>
2022-11-18 13:40:47 +0900

    [Bronze II] Title: 블랙잭, Time: 104 ms, Memory: 30840 KB -BaekjoonHub

---
+++ 백준/Bronze/2798. 블랙잭/블랙잭.py
@@ -0,0 +1,16 @@
+# 3. 블랙잭 (브2)
+
+import sys
+input = sys.stdin.readline
+
+N, M = map(int, input().split())
+card = list(map(int, input().split()))
+
+result = 0
+for i in range (N) :
+  for j in range (i+1, N):
+    for k in range (j+1, N):
+      if card[i]+card[j]+card[k] > result and card[i]+card[j]+card[k] <= M :
+        result = card[i]+card[j]+card[k]
+
+print(result)
