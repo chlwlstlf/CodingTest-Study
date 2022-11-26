2f251f1ed02e8e3ead6176d6a537a334ad77e0d4
최진실 <63334368+chlwlstlf@users.noreply.github.com>
2022-11-26 15:26:51 +0900

    Update 숫자 야구.py

--- 백준/Silver/2503. 숫자 야구/숫자 야구.py
+++ 백준/Silver/2503. 숫자 야구/숫자 야구.py
@@ -1,5 +1,4 @@
-# 16. 숫자 야구 (실3) 틀림
-
+# 16. 숫자 야구 (실3) 맞음
 
 import sys
 input = sys.stdin.readline
@@ -13,10 +12,14 @@ for i in range (N):
 result = 0
 for i in range (111, 1000):
   i = list(map(int,str(i)))
+
+  #숫자에 0이 포함되어 있으면 제외
   if 0 in i :
     continue
+
+  #서로 다른 세 숫자일 때
   if i[0] != i[1] and i[1] != i[2] and i[2] != i[0] :
-    f = 0
+    cnt = 0
     for j in range (N):
       strike = 0
       ball = 0
@@ -25,8 +28,10 @@ for i in range (111, 1000):
           strike += 1
         if i[k] in num[j][0] and i[k] != num[j][0][k] :
           ball += 1
+      #strike와 ball 결과값이 같을 때 cnt+1
       if strike == num[j][1] and ball == num[j][2]:
-        f += 1
-    if f == N :
+        cnt += 1
+    #strike와 ball 결과값이 전부 다 같을 때 result+1
+    if cnt == N :
       result += 1
 print(result)
