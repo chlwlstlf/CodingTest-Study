e9c5489c9f56c1e2468257a321563b89842587f7
최진실 <63334368+chlwlstlf@users.noreply.github.com>
2023-05-06 15:27:43 +0900

    [Silver IV] Title: 요세푸스 문제, Time: 56 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/1158. 요세푸스 문제/요세푸스 문제.py
+++ 백준/Silver/1158. 요세푸스 문제/요세푸스 문제.py
@@ -1,24 +1,20 @@
 # 3. 요세푸스 문제 (실4)
 
 import sys
-from collections import deque
 input = sys.stdin.readline
 
 N, K = map(int, input().split())
-
-q = deque()
-for i in range(N):
-  q.append(i+1)
+arr = [i+1 for i in range(N)] 
 
 result = '<'
 idx = 0
+end = N
 for i in range(N):
-  #K-1 명은 넘어감
-  for j in range(K-1):
-    q.append(q.popleft())
+  idx = (idx+K-1)%end
+  result += str(arr[idx])
+  end -= 1
+  arr.pop(idx)
 
-  #K 번째 사람 result에 넣기
-  result += str(q.popleft())
   if i == N-1:
     break
   result += ', '
