09872f7850838e261495ecf0d782664cf1dc6147
최진실 <63334368+chlwlstlf@users.noreply.github.com>
2023-08-04 01:04:13 +0900

    [Gold IV] Title: 타일 채우기, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Gold/2133. 타일 채우기/타일 채우기.py
@@ -0,0 +1,13 @@
+# 3. 타일 채우기 (골4)
+# 힌트봄
+
+N = int(input())
+
+dp = [0]*(N+1)
+dp[0] = 1
+for i in range(2, N+1, 2):
+  dp[i] = dp[i-2] * 3
+  for j in range(0, i-2, 2):
+    dp[i] += dp[j]*2
+
+print(dp[N])
