c5773e116f36a7505a619ad7490acb011c54daff
최진실 <63334368+chlwlstlf@users.noreply.github.com>
2023-10-17 15:09:23 +0900

    [level 1] Title: [카카오 인턴] 키패드 누르기, Time: 0.35 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/1/67256. ［카카오 인턴］ 키패드 누르기/［카카오 인턴］ 키패드 누르기.py
+++ 프로그래머스/1/67256. ［카카오 인턴］ 키패드 누르기/［카카오 인턴］ 키패드 누르기.py
@@ -1,9 +1,11 @@
+# 2. [카카오 인턴] 키패드 누르기
+
 def solution(numbers, hand):
     # 디폴트값 설정
     keypad = {1: (0, 0), 2: (0, 1), 3: (0, 2), 
               4: (1, 0), 5: (1, 1), 6: (1, 2), 
               7: (2, 0), 8: (2, 1), 9: (2, 2),
-             '*': (3, 0), 0: (3, 1), '#': ( 3, 2)}
+             '*': (3, 0), 0: (3, 1), '#': (3, 2)}
     fixed = {1: 'L', 3: 'R', 4: 'L', 6: 'R', 7: 'L', 9: 'R'}
     current = {'L': '*', 'R': '#'}
     if hand == 'left': flag = 'L'
@@ -22,18 +24,13 @@ def solution(numbers, hand):
             y = keypad[n][1]
             L_dist = abs(keypad[current['L']][0]-x) + abs(keypad[current['L']][1]-y)
             R_dist = abs(keypad[current['R']][0]-x) + abs(keypad[current['R']][1]-y)
-            if flag == 'L':
-                if L_dist > R_dist:
-                    answer += 'R'
-                    current['R'] = n
-                else:
-                    answer += 'L'
-                    current['L'] = n
+            if L_dist < R_dist:
+                answer += 'L'
+                current['L'] = n
+            elif L_dist > R_dist:
+                answer += 'R'
+                current['R'] = n
             else:
-                if L_dist < R_dist:
-                    answer += 'L'
-                    current['L'] = n
-                else:
-                    answer += 'R'
-                    current['R'] = n  
+                answer += flag
+                current[flag] = n
     return answer
