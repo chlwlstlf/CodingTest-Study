2c286cfa91016311ec58663a0f0061ee132f6ab5
최진실 <jinsil378900@gmail.com>
2025-02-13 02:29:13 +0900

    [level 1] Title: 유연근무제, Time: 4.00 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/1/388351. 유연근무제/유연근무제.py
@@ -0,0 +1,19 @@
+def changeTime(time):
+    return (time//100)*60 + time%100
+
+def solution(schedules, timelogs, startday):
+    N = len(schedules)
+    answer = 0
+    for i in range(N):
+        schedule = changeTime(schedules[i])
+        cnt = 0
+        for j in range(7):
+            today = (startday+j)%7
+            if today == 6 or today == 0:
+                continue
+            timelog = changeTime(timelogs[i][j])
+            if timelog <= schedule+10:
+                cnt += 1
+        if cnt == 5:
+            answer += 1
+    return answer
