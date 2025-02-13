effd82432b64eb96e422351e894df7b4eecb5f53
최진실 <jinsil378900@gmail.com>
2025-02-13 16:42:54 +0900

    [level 2] Title: 완전범죄, Time: 23.85 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/2/389480. 완전범죄/완전범죄.py
@@ -0,0 +1,31 @@
+from collections import deque
+import sys
+input = sys.stdin.readline
+
+def bfs(x, y, info, dp):
+    N, M = len(dp), len(dp[0])
+    q = deque()
+    q.append((x, y))
+    for i in range(len(info)):
+        n = len(q)
+        for j in range(n):
+            x, y = q.popleft()
+            nx = x + info[i][0]
+            ny = y + info[i][1]
+            if 0 <= nx < N and dp[nx][y] != i+1:
+                dp[nx][y] = i+1
+                q.append((nx, y))
+            if 0 <= ny < M and dp[x][ny] != i+1:
+                dp[x][ny] = i+1
+                q.append((x, ny))
+
+
+def solution(info, n, m):
+    N = len(info)
+    dp = [[0]*m for _ in range(n)]
+    bfs(0, 0, info, dp)
+    for i in range(len(dp)):
+        if dp[i].count(N) > 0:
+            return i
+
+    return -1
